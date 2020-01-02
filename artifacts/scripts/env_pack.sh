fcc2ca258d78c1e88362613c27e16640c508aca9
Pawel Piwosz <pawel@pawelpiwosz.net>
2020-01-02 21:10:11 +0100

    fix path

--- artifacts/scripts/env_pack.sh
+++ artifacts/scripts/env_pack.sh
@@ -4,4 +4,4 @@ cd ../airly/lib/python3.6/site-packages/
 zip -g -r ../../../../package/CloudControlMainFunction.zip . 
 cd ../../../../../py/
 pwd && ls -al
-zip -g ../artifact/package/CloudControlMainFunction.zip CloudControlMainFunction.py
+zip -g ../artifacts/package/CloudControlMainFunction.zip CloudControlMainFunction.py
